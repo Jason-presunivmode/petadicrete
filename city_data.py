28edbd1bfd9eb047f0a683003092090dbb544fcc
Jason-presunivmode <jason.pham@student.president.ac.id>
2024-12-13 15:05:14 +0700

    Create city_data.py

---
+++ city_data.py
@@ -0,0 +1,53 @@
+# city_data.py
+
+provinces_data = {
+    "West Java": [
+        {"name": "Bandung", "lat": -6.9147, "lon": 107.6098},
+        {"name": "Bogor", "lat": -6.5950, "lon": 106.8167},
+        {"name": "Bekasi", "lat": -6.2383, "lon": 106.9756},
+        {"name": "Cimahi", "lat": -6.8794, "lon": 107.5887},
+        {"name": "Tasikmalaya", "lat": -7.3841, "lon": 108.2232},
+        {"name": "Cirebon", "lat": -6.7320, "lon": 108.5523},
+        {"name": "Garut", "lat": -7.2119, "lon": 107.9063},
+        {"name": "Sukabumi", "lat": -6.9183, "lon": 106.9260},
+        {"name": "Karawang", "lat": -6.3030, "lon": 107.3106},
+    ],
+    "Banten": [
+        {"name": "Serang", "lat": -6.1209, "lon": 106.1505},
+        {"name": "Tangerang", "lat": -6.2024, "lon": 106.6527},
+        {"name": "Cilegon", "lat": -6.0025, "lon": 106.0111},
+        {"name": "Pandeglang", "lat": -6.3088, "lon": 106.1062},
+        {"name": "Lebak", "lat": -6.5645, "lon": 106.2522},
+    ],
+    "Jakarta": [
+        {"name": "Central Jakarta", "lat": -6.1865, "lon": 106.8349},
+        {"name": "West Jakarta", "lat": -6.1683, "lon": 106.7584},
+        {"name": "East Jakarta", "lat": -6.2251, "lon": 106.9004},
+        {"name": "South Jakarta", "lat": -6.2686, "lon": 106.7790},
+        {"name": "North Jakarta", "lat": -6.1214, "lon": 106.8597},
+    ],
+    "Central Java": [
+        {"name": "Semarang", "lat": -6.9667, "lon": 110.4167},
+        {"name": "Surakarta", "lat": -7.5755, "lon": 110.8243},
+        {"name": "Purwokerto", "lat": -7.4218, "lon": 109.2344},
+        {"name": "Magelang", "lat": -7.4700, "lon": 110.2177},
+        {"name": "Tegal", "lat": -6.8697, "lon": 109.1215},
+        {"name": "Pekalongan", "lat": -6.8881, "lon": 109.6753},
+    ],
+    "East Java": [
+        {"name": "Surabaya", "lat": -7.2575, "lon": 112.7521},
+        {"name": "Malang", "lat": -7.9787, "lon": 112.5617},
+        {"name": "Madiun", "lat": -7.6295, "lon": 111.5235},
+        {"name": "Jember", "lat": -8.1724, "lon": 113.6995},
+        {"name": "Blitar", "lat": -8.0950, "lon": 112.1611},
+        {"name": "Kediri", "lat": -7.8168, "lon": 112.0116},
+        {"name": "Banyuwangi", "lat": -8.2186, "lon": 114.3694},
+    ],
+    "DI Yogyakarta": [
+        {"name": "Yogyakarta", "lat": -7.7972, "lon": 110.3688},
+        {"name": "Sleman", "lat": -7.7330, "lon": 110.4026},
+        {"name": "Bantul", "lat": -7.8880, "lon": 110.3300},
+        {"name": "Kulon Progo", "lat": -7.8352, "lon": 110.1655},
+        {"name": "Gunung Kidul", "lat": -8.0315, "lon": 110.6167},
+    ],
+}
